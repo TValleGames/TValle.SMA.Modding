31d26e02a504095742a2dad5e2a49b8bf28ac1ee
TValle <tvallegames@gmail.com>
2025-05-17 00:39:32 -0500

    2022.62

--- Assets/TValle/Tools/Runtime/Characters/BuffAndDebuff/BuffOnKarma.cs
+++ Assets/TValle/Tools/Runtime/Characters/BuffAndDebuff/BuffOnKarma.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.WSA;
 
 namespace Assets.TValle.Tools.Runtime.Characters.BuffAndDebuff
 {

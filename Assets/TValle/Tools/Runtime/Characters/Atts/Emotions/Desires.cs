2e6c874806915693f0301fc83a9a445926088eb6
TValle <tvallegames@gmail.com>
2025-05-18 04:33:38 -0500

    2022.63

--- Assets/TValle/Tools/Runtime/Characters/Atts/Emotions/Desires.cs
+++ Assets/TValle/Tools/Runtime/Characters/Atts/Emotions/Desires.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 namespace Assets.TValle.Tools.Runtime.Characters.Atts.Emotions
 {
-    public enum Desires 
+    public enum Desires
     {
-       
-        All=1,
-        None=0,
+
+        All = -1,
+        None = 0,
         Crotch,
         Ass,
         Breast,
